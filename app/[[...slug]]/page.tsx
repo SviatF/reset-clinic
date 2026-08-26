33e04f98d4c94eca33d7a2fbc72f297a22a6ab06
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-08-26 02:14:26 -0700

    Pass legacy route into page renderer

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -42,7 +42,7 @@ export default async function Page({ params }: Props) {
           dangerouslySetInnerHTML={{ __html: jsonLd(schema) }}
         />
       ))}
-      <LegacyPage data={data} mobile={mobile} />
+      <LegacyPage data={data} mobile={mobile} route={key} />
     </>
   );
 }
