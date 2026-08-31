00e897a32b354c0675a55af091ec7f75ed6a79c3
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-08-31 11:24:37 -0700

    Render GTM as native head script

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,17 +80,20 @@ export const viewport: Viewport = {
   themeColor: "#29201B",
 };
 
+const GTM_HEAD_SCRIPT = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-THZNMKV3');`;
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="uk-UA">
       <head>
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-THZNMKV3');`}
-        </Script>
+        <script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{ __html: GTM_HEAD_SCRIPT }}
+        />
 
         <Script id="meta-pixel-general" strategy="beforeInteractive">
           {`!function(f,b,e,v,n,t,s)
